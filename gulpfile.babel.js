660b70b8a2e629b343f6ccbae8b732757afea394
tonky0110 <tonky0110@gmail.com>
2019-07-06 00:17:07 +0900

    3.0 gulp-image , img파일 처리 task && watch img

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,28 @@ import gulp from 'gulp';
 import gpug from 'gulp-pug';
 import del from 'del';
 import ws from 'gulp-webserver';
+import image from 'gulp-image';
 
+// routes
 const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
     }
 }
+
+// tasks
 const pug = () => 
     gulp
         .src(routes.pug.src)
         .pipe(gpug())
         .pipe(gulp.dest(routes.pug.dest));
-
 const clean = () => del(["build"]);
-
 const webserver = () => 
     gulp
         .src("build")
@@ -27,11 +33,17 @@ const webserver = () =>
             open: true
         }));
 
+const img = () => 
+    gulp
+        .src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest));
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, img);
 };
-
-const prepare = gulp.series([clean]);
+// series
+const prepare = gulp.series([clean, img]);
 const assets = gulp.series([pug]);
 const postDev = gulp.series([webserver, watch]);
 
